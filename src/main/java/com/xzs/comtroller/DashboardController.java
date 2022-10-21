cdb4aa083ee8dffeacb537b64be9c4fd141d2b26
kach-13 <2595931767@qq.com>
2022-10-21 16:26:38 +0800

    Updata

--- src/main/java/com/xzs/comtroller/DashboardController.java
+++ src/main/java/com/xzs/comtroller/DashboardController.java
@@ -94,7 +94,7 @@ public class DashboardController {//主页
 
             index.setDoQuestionCount(String.valueOf(90));
             List<String> list2 = new ArrayList<>();
-            list2.add("999");
+            list2.add("990");
             index.setMothDayUserActionValue(list2);
             index.setMothDayDoExamQuestionValue(list2);
             index.setMothDayText(list2);
